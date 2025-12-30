54d017517ff02c37a2827aebdaddebbe60d79468
Divine Chile <divinechile16@gmail.com>
2025-12-30 16:38:13 +0100

    Refactored files

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-export const PORT = process.env.PORT || 4000;
+export const PORT = process.env.PORT || 3000;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
 if (!TOKEN_SECRET) {
